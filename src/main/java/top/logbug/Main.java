0ef34a233bc27504f594be04c6ba7f7139f7e178
logbug <logbug@aliyun.com>
2021-12-14 02:07:47 +0800

    握手

--- src/main/java/top/logbug/Main.java
+++ src/main/java/top/logbug/Main.java
@@ -1,13 +1,11 @@
 package top.logbug;
 
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ByteUtil;
 import cn.hutool.core.util.HexUtil;
 import cn.hutool.core.util.StrUtil;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Locale;
@@ -18,30 +16,56 @@ public class Main {
         ServerSocket serverSocket= new ServerSocket(3306);
         while (true){
             Socket socket = serverSocket.accept();
-            String greating="""
+            Thread thread = new Thread(
+                    () -> {
+
+                        try {
+                            System.out.println("new connection: " + socket);
+                            String greeting="""
                     4a 00 00 00 0a 38 2e 30 2e 31 39 00 fd 02 00 00
                     53 01 71 2b 4b 6e 37 52 00 ff ff ff 02 00 ff c7
                     15 00 00 00 00 00 00 00 00 00 00 4b 4f 61 4e 31
                     0e 0e 6b 39 5d 56 0a 00 63 61 63 68 69 6e 67 5f
                     73 68 61 32 5f 70 61 73 73 77 6f 72 64 00
                                         
-                    """.replaceAll("\s","");
-            byte[] bytes = HexUtil.decodeHex(greating);
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(bytes);
-            outputStream.flush();
+                    """.replaceAll("[\s\n]","");
+                            byte[] bytes = HexUtil.decodeHex(greeting);
+                            OutputStream outputStream = socket.getOutputStream();
+                            outputStream.write(bytes);
+                            outputStream.flush();
+                            System.out.println("write greeting  "+greeting);
 
 
-            BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String s = reader.readLine();
-            System.out.println("s = " + s);
-            String ok= """
+                            byte[] readBytes=new byte[1024];
+                            InputStream ins = socket.getInputStream();
+
+                            ins.read(readBytes);
+                            String s= new String(readBytes);
+                            System.out.println("read msg:  " + s);
+                            String ok= """
                     07 00 00 05 00 00 00 02 00 00 00
-                    """.replaceAll("\s","");
-            bytes = HexUtil.decodeHex(ok);
-            System.out.println("ok = " + ok);
-            outputStream.write(bytes);
-            outputStream.flush();
+                    """.replaceAll("[\s\n]","");
+                            bytes = HexUtil.decodeHex(ok);
+                            outputStream.write(bytes);
+                            outputStream.flush();
+                            System.out.println("write ok " + ok);
+
+                            byte[] readBytes2=new byte[1024];
+//
+                            ins.read(readBytes2);
+
+                            int packetLength = ByteUtil.bytesToInt(ArrayUtil.sub(readBytes2,0,3));
+                            System.out.println("packetLength = " + packetLength);
+                            int packetNumber = ByteUtil.bytesToInt(ArrayUtil.sub(readBytes2,3,4));
+                            System.out.println("packetNumber = " + packetNumber);
+                            System.out.println("sql = " + new String(ArrayUtil.sub(readBytes2,4,1024)));
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+            });
+            thread.start();
+
 
         }
     }
